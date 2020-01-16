8809cc4b8c9254667333088d76db28dc9380c9c0
Grace Shih <vocalise@gmail.com>
2020-01-16 06:26:08 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -9,7 +9,7 @@ class SightingsController < ApplicationController
     if sighting
       render json: sighting.to_json(:include => {:bird => {:only => [:name, :species]},
         :location => {:only => [:latitude, :longitude]}
-        }, :except => [:updated_at]()
+        }, :except => [:updated_at])
     else
       render json: {message: 'No sighting found with that id' }
   end
